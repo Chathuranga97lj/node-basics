661dc5b2f2a62bbe3278fa7aa60f326a2ba6f440
Chathuranga97lj <chathuit97@gmail.com>
2022-09-04 18:47:10 +0530

    set user

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 // create server
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.end(`
-        <html>
-            <head>
-                <title>First Node App</title>
-            </head>
-            <body>
-                <h1>Welcome to my node app</h1>
-            </body>
-        </html>
-    `);
+    switch(req.url) {
+        case '/':
+            res.end(`
+                <html>
+                <head>
+                    <title>First Node App</title>
+                </head>
+                <body>
+                    <h1>Welcome to my node app</h1>
+                </body>
+                </html>
+            `);
+            break;
+
+        case '/admin':
+            res.end('Welcome to the admin page');
+            break;
+        
+        case '/user':
+            res.end('Welcome to user page');    
+    }
+    
 });
 
 // set port
