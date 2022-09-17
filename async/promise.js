69eb93fd314344619f5d6ab8058de0583b09518e
Chathuranga97lj <chathuit97@gmail.com>
2022-09-17 22:17:26 +0530

    add master promiss1

--- async/promise.js
+++ async/promise.js
@@ -1,20 +1,50 @@
-const check = false;
+const movies = require('./movies.json');
+const reviews = require('./reviews.json');
+const users = require('./users.json');
 
-const promise = new Promise((resolve, reject) => {
-    if(check){
-        resolve("Done");
-    } else {
-        reject("Not Done");
-    }
-});
+const getMovies = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const movie = movies.find((movie) => movie.id ===id);
+            if(movie){
+                resolve(movie);
+            } else {
+                reject('Not found!');
+            }
+        }, 1000);
+    });
+};
 
-// console.log(promise);
-//to get result and haddle error 
+const getReviews = (movieid, cb) => {
+    setTimeout(() => {
+        const review = reviews.find((review) => review.movie_id === movieid);
+        cb(review);
+    }, 1000);
+};
 
-promise
-    .then((res) => {
-        console.log(res);
+const getUsers = (name, cb) => {
+    setTimeout(() => {
+        const user = users.find(user => user.name === name);
+        cb(user);
+    }, 1000);
+};
+
+
+// // calback pattern 1 (Call back hell- not use this  type)
+// getMovies(3, (movie) => {
+//     // console.log(movie);
+//     getReviews(movie.id, (review) => {
+//         // console.log(review);
+//         getUsers(review.reviewer, (user) => {
+//             console.log(user);
+//         });
+//     });
+// })
+
+getMovies(1)
+    .then(movie => {
+        console.log(movie);
     })
-    .catch((err) => {
-        console.log(err);
+    .catch(err => {
+        console.log(err)
     })
