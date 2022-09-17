ddc20fe81a964c433dc65a188878076b29a5f170
Chathuranga97lj <chathuit97@gmail.com>
2022-09-17 20:33:07 +0530

    add callback pattern1

--- async/callback.js
+++ async/callback.js
@@ -1,3 +1,23 @@
 const movies = require('./movies.json');
 const reviews = require('./reviews.json');
 const users = require('./users.json');
+
+const getMovies = (id, cb) => {
+    setTimeout(() => {
+        const movie = movies.find((movie) => movie.id ===id);
+        cb(movie);
+    }, 1000);
+}
+
+const getReviews = () => {
+
+}
+
+const getUsers = () => {
+
+}
+
+// calback pattern 1
+getMovies(3, (movie) => {
+    console.log(movie);
+})
