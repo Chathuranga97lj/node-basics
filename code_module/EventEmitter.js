651a41231a77f3db7e41b9728c9b8f9084991602
Chathuranga97lj <chathuit97@gmail.com>
2022-09-18 14:35:59 +0530

    add emitter2

--- code_module/EventEmitter.js
+++ code_module/EventEmitter.js
@@ -3,9 +3,14 @@ const {EventEmitter} = require('events');
 const myEmitter = new EventEmitter();
 
 // listener
-myEmitter.on('greeting', () => {
-    console.log('Hello world');
+myEmitter.on('greeting', (name) => {
+    console.log(`Hello ${name} !`);
 });
 
+myEmitter.on('greeting', () => {
+    console.log("Hello again !");
+})
+
+
 // emitter
-myEmitter.emit('greeting');
+myEmitter.emit('greeting', 'Chathuranga');
