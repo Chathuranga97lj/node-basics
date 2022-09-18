193c233123ac81a0555217dd6d7c2408752e4e43
Chathuranga97lj <chathuit97@gmail.com>
2022-09-18 17:31:48 +0530

    add file system2

--- code_module/fs.js
+++ code_module/fs.js
@@ -1,12 +1,20 @@
-const {opendir} = require('fs');
+const {opendir, stat} = require('fs');
 const {promisify} = require('util');
 
 const oDir = promisify(opendir);
+const getStat = promisify(stat);
 
-oDir('./')
-    .then(async (dir) => {
-        for await(let dirent of dir) {
-            console.log(dirent.name);
-        };
+// see what are the files in directory
+// oDir('./')
+//     .then(async (dir) => {
+//         for await(let dirent of dir) {
+//             console.log(dirent.name);
+//         };
+//     })
+//     .catch()
+
+getStat('./EventEmitter.js')
+    .then((stat) => {
+        console.log(stat);
     })
-    .catch()
+    .catch(err => console.log(err));
