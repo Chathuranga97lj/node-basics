0bb5e253e521a4618c143a34df4bd3e325ac56a9
Chathuranga97lj <chathuit97@gmail.com>
2022-09-18 14:50:37 +0530

    add emitter3

--- code_module/EventEmitter.js
+++ code_module/EventEmitter.js
@@ -12,6 +12,23 @@ myEmitter.on('greeting', () => {
     console.log("Hello again !");
 })
 
-
 // emitter
 myEmitter.emit('greeting', 'Chathuranga');
+
+
+// listen server
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('listening', () => {
+    setImmediate(() => {
+        console.log('Server is listening now');
+    })
+});
+
+server.on('listening', () => {
+    console.log('Server is listening now again');
+});
+
+server.listen(3000);
