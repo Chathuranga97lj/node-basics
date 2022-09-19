76d8758ad585e472ffeca4ceaa8d8cdc5e7f670e
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 14:29:41 +0530

    add path1

---
+++ code_module/module/mode.js
@@ -0,0 +1,2 @@
+console.log(__dirname); // output path of module
+console.log(process.cwd()); // output path of code_module - process path
---
+++ code_module/path.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const mode = require('./module/mode');
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(process.cwd());
