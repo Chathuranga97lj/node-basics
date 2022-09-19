4438fc11a58bb5aa1842a0987a8ff41604ab3c64
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 14:19:37 +0530

    add stream2

--- code_module/streams.js
+++ code_module/streams.js
@@ -6,10 +6,21 @@
  * duplex
  */
 
-const {createReadStream, createWriteStream} = require('fs');
+// const {createReadStream, createWriteStream} = require('fs');
 
-const readStream = createReadStream('./text.txt');
+// const readStream = createReadStream('./text.txt');
 
-const writeStream = createWriteStream('./text2.txt');
+// const writeStream = createWriteStream('./text2.txt');
 
-readStream.pipe(writeStream);
+// readStream.pipe(writeStream);
+
+
+const {createServer} = require('http');
+
+const server = createServer();
+
+server.listen(5000);
+
+server.on('connection', (socket) => {
+    console.log(socket);
+});
