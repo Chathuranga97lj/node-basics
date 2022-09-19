1e671c73f738b1a4bed699be42ef50323d3be3a5
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 14:57:16 +0530

    add path files

--- code_module/path.js
+++ code_module/path.js
@@ -1,7 +1,16 @@
 const path = require('path');
-const mode = require('./module/mode');
+// const mode = require('./module/mode');
 
 // console.log(__dirname);
 // console.log(__filename);
 
 // console.log(process.cwd());
+
+const base = path.basename(__filename);
+console.log(base);
+
+const ext = path.extname(__filename); // extention
+console.log(ext);
+
+const parse = path.parse(__filename);
+console.log(parse);
