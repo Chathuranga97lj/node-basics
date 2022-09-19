86143df3a70795dd32f3c934de586f2f60a8155e
Chathuranga97lj <chathuit97@gmail.com>
2022-09-19 15:01:03 +0530

    add path3

--- code_module/path.js
+++ code_module/path.js
@@ -1,16 +1,23 @@
 const path = require('path');
-// const mode = require('./module/mode');
+const mode = require('./module/mode');
 
 // console.log(__dirname);
 // console.log(__filename);
 
 // console.log(process.cwd());
 
-const base = path.basename(__filename);
-console.log(base);
+// const base = path.basename(__filename);
+// console.log(base);
 
-const ext = path.extname(__filename); // extention
-console.log(ext);
+// const ext = path.extname(__filename); // extention
+// console.log(ext);
 
-const parse = path.parse(__filename);
-console.log(parse);
+// const parse = path.parse(__filename);
+// console.log(parse);
+
+
+const {readFile} = require('fs');
+
+readFile(path.join(__dirname, 'module', 'mode.js'), 'utf8', (err, data) => {
+    console.log(data);
+})
